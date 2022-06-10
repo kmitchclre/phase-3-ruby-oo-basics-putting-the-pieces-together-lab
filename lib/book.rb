fec4c854012e61419f325f98e58529111a558194
Kenny Clare <kmitchellclare@gmail.com>
2022-06-10 12:47:24 -0700

    commit tree 8152f491575c247968d8b0e6e9c71330ef1315f6

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+title = Book.new("And Then there Were None")
+author = Book.new("Agatha Christie")
+genre = Book.new("Mystery")
